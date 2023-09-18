4a74d78ae75564d25f8e5e9df3fae3bc7aeadc0f
preferldev <>
2023-09-18 13:15:45 +0100

    switch back to article

--- src/lib/Articles.svelte
+++ src/lib/Articles.svelte
@@ -7,14 +7,14 @@
 
 <div class="grid">
 	{#each articles as article}
-		<details class="article">
-			<summary><strong>{article.title}</strong></summary>
+		<article class="article">
+			<header><strong>{article.title}</strong></header>
 			<img class="article-image" src={article.image} alt={article.imageAlt} />
 			<footer>
 				<p>{article.description}</p>
 				<a class="article-button" href={article.link} role="button">{article.buttonText}</a>
 			</footer>
-		</details>
+		</article>
 	{/each}
 </div>
 
